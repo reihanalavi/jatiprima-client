c75d95c28805329745be458ac29a5ab0d768a5d3
reihanalavi <reihanalavi@gmail.com>
2023-04-11 05:05:15 +0700

    changing next config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['jatiprima-server.s3.ap-southeast-1.amazonaws.com']
+    domains: [
+      'jatiprima-server.s3.ap-southeast-1.amazonaws.com',
+      'jatiprima-foto.s3.ap-southeast-2.amazonaws.com'
+    ]
   }
 }
 
